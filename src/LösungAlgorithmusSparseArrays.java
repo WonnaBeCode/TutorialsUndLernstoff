966c4fcd5050d9df1c0ebd563a4e3cd5ec473255
Martin Rinder <m.rinder@tikl.local>
2017-11-23 14:40:30 +0100

    First commit

--- src/LösungAlgorithmusSparseArrays.java
+++ src/LösungAlgorithmusSparseArrays.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
 
 public class LösungAlgorithmusSparseArrays {
+	
+	
+   
+    	private static Scanner in;
 
-	
-	
-	
-}
+		public static void main(String[] args) {
+            
+            in = new Scanner (System.in); 
+    	       int n = in.nextInt ();
+    	           String s[] = new String [n];
+    		             for (int i = 0; i< s.length; i++) {
+    			
+                             s [i] = in.next ();
+    			
+    		}
+    		int q = in.nextInt();
+    		int count [] = new int [q];
+    		  for (int i = 0; i < q; i++) {
+    			String ss = in.next ();
+    			     for (int j =0; j < s.length; i++) {
+    				    if (s[j].equals(ss)) {
+    					   count [i] ++;
+                        }
+    				}
+    			}
+    			for (int i = 0; i < count.length; i++) {
+    				System.out.println (count [i]);
+    				
+    			}
+    		}
+    	}
